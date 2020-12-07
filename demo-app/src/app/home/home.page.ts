9dded9040d452c5a33413fdfd5dc3c432aa3a611
cdufour <opusidea@gmail.com>
2020-12-07 15:39:10 +0100

    J1 TP

--- demo-app/src/app/home/home.page.ts
+++ demo-app/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { TestComponent } from '../components/test/test.component';
 
 interface Student {
   id: number;
@@ -19,13 +21,35 @@ export class HomePage {
     { id: 232, name: "Noémie", grade: 18, isProjectDone: true, comment: "Brillante, tout simplement" },
     { id: 34, name: "Umberto", grade: 6.5, isProjectDone: false, comment: "Dissipé, pertubateur" }
   ]
-
+  numFastStudent: number;
   message: string = "";
 
-  constructor() {}
+  constructor(private popoverCtrl: PopoverController) {
+    this.numFastStudent = this.getDoneProject().length;
+  }
 
   onClick(index: number) {
     this.message = this.students[index].comment;
+  }
+
+  onChange(e: any, index: number) {
+    let checked = e.target.checked;
+    // mise à jour de la source de données
+    this.students[index].isProjectDone = checked;
+    this.numFastStudent = this.getDoneProject().length;
+  }
+
+  async testPopover() {
+    let popover = await this.popoverCtrl.create({
+      component: TestComponent
+    });
+    await popover.present();
+  }
+
+  /* Retourne les étudiants ayant terminé leur projet */
+  private getDoneProject(): Student[] {
+    return this.students
+      .filter(student => student.isProjectDone == true)
   }
 
 }
